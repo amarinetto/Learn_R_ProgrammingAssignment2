6ba7d437c9ae635dabb69f1b8cdd4eb6d95c9bed
amarinetto <amarinetto@gmail.com>
2016-12-25 21:56:47 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -54,15 +54,3 @@ cacheSolve <- function(x, ...) {
     #and return it
     invert
 }
-
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
